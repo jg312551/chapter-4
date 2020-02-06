37afff1899db33fc223e749845a9433773e94f0b
jg312551 <48728410+jg312551@users.noreply.github.com>
2020-02-06 12:42:22 -0600

    update

--- src/gameZone/Die.java
+++ src/gameZone/Die.java
@@ -1,16 +1,18 @@
 package gameZone;
 
 public class Die {
-        public static void main(String[] args){
-           int value=((int)(Math.random() *100)%6 + 1);
+    public static void main(String[] args){
+        int value=((int)(Math.random() *100)%6 + 1);
 
 
-            toDie one = new toDie();
-            toDie two = new toDie();
+        toDie one = new toDie();
+        toDie two = new toDie();
 
-            
 
-        }
+
+
+
     }
+}
 
 
